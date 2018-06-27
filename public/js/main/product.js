8e2f4653f6f8720c014fa67750cae1c5a4d3e29a
nhvn <nguyenhoangvannha@gmail.com>
2018-06-28 00:00:17 +0700

    Product details

--- public/js/main/product.js
+++ public/js/main/product.js
@@ -1,6 +1,6 @@
-var app = angular.module('app.product',['ngRoute']);
+var app = angular.module('app.product', ['ngRoute']);
 
-app.controller('productController', ['$scope', 'svProduct', '$routeParams' , '$location', ProductCtrl]);
+app.controller('productController', ['$scope', 'svProduct', '$routeParams', '$location', ProductCtrl]);
 function ProductCtrl($scope, svProduct, $routeParams, $location) {
     $scope.product = {};
     var url = $location.absUrl().split('/');
@@ -10,30 +10,49 @@ function ProductCtrl($scope, svProduct, $routeParams, $location) {
     console.log(ID);
     $scope.products = [];
     $scope.companyProducts = [];
+    $scope.sameTypeProducts = [];
+
+    function shuffle(array) {
+        var m = array.length, t, i;
+        // Chừng nào vẫn còn phần tử chưa được xáo trộn thì vẫn tiếp tục
+        while (m) {
+            // Lấy ra 1 phần tử
+            i = Math.floor(Math.random() * m--);
+            // Sau đó xáo trộn nó
+            t = array[m];
+            array[m] = array[i];
+            array[i] = t;
+        }
+        return array;
+    }
     svProduct.getProduct(ID).then(function (result) {
         $scope.product = result.data[0];
-        console.log($scope.product);
+        svProduct.getSameTypeProducts($scope.product.LOAI).then(function (result) {
+            $scope.sameTypeProducts = result.data;
+            shuffle($scope.sameTypeProducts);
+        }, function (err) {
+            console.log(err);
+        });
     }, function (err) {
-        console.log(" PS" , err);
+        console.log(" PS", err);
     });
     svProduct.getProducts().then(function (result) {
         $scope.products = result.data;
-        console.log($scope.products);
     }, function (err) {
         console.log(err);
     });
     svProduct.getCompanyProducts(COMPANY).then(function (result) {
         $scope.companyProducts = result.data;
-        console.log('companyProducts',$scope.companyProducts);
+        shuffle($scope.companyProducts);
     }, function (err) {
         console.log(err);
     });
     svProduct.getCompanies().then(function (result) {
         result.data.forEach(element => {
-            if(element.NHASANXUAT != 'undefined'){
+            if (element.NHASANXUAT != 'undefined') {
                 $scope.companies.push(element);
             }
         });
-        console.log($scope.companies);
     });
+
 }

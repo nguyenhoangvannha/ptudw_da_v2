06f2b914726b3696e81f5ddc7e679cafb30f5f46
nhvn <nguyenhoangvannha@gmail.com>
2018-06-28 16:52:52 +0700

    Merge sign log

--- app.js
+++ app.js
@@ -11,6 +11,11 @@ var productRouter = require('./routes/product');
 var shopRouter = require('./routes/shop');
 var searchRouter = require('./routes/search');
 var GovernanceRouter = require('./routes/Governance');
+//////////////Nhân///////////////////////////
+var signlogRouter = require('./routes/signlog');
+var session = require('express-session');
+var MySQLStore = require('express-mysql-session')(session);
+///////////////////Nhân////////////////////////////////////
 
 var app = express();
 
@@ -18,6 +23,33 @@ var port = process.env.PORT || 3000;
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
+
+//////////////////////////Nhân///////////////////////
+var sessionStore = new MySQLStore({
+	host: 'localhost',
+	port: 3306,
+	user: 'root',
+	password: '',
+	database: 'linhkienfitdb',
+	createDatabaseTable: true,
+	schema: {
+		tableName: 'sessions',
+		columnNames: {
+			session_id: 'session_id',
+			expires: 'expires',
+			data: 'data'
+		}
+	}
+});
+
+app.use(session({
+	key: 'session_cookie_name',
+	secret: 'session_cookie_secret',
+	store: sessionStore,
+	resave: false,
+	saveUninitialized: false
+}));
+///////////////////////////Nhân//////////////////////////
 
 app.use(logger('dev'));
 app.use(express.json());
@@ -34,6 +66,7 @@ app.use('/product', productRouter);
 app.use('/shop', shopRouter);
 app.use('/search',searchRouter);
 app.use('/Governance',GovernanceRouter);
+app.use('/signlog', signlogRouter);
 app.listen(port, function () {
   console.log('App listeing on port', port);
 });

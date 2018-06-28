27ae2eea2d5f3393680dba451e05f06941635fc0
nhvn <nguyenhoangvannha@gmail.com>
2018-06-28 17:54:46 +0700

    Add login header to shop product

--- routes/product.js
+++ routes/product.js
@@ -2,9 +2,29 @@ var express = require('express');
 var router = express.Router();
 
 router.get('/:ID', function (req, res, next) {
-    res.render('product', {ID: req.params.ID});
+    var log=req.session.isLogged;
+	var name=req.session.username;
+	var admin = false;
+	var vm = {
+		isAdmin: admin,
+		layout: false,
+		isLogged: log,
+		showError: false,
+		username: name
+	};
+    res.render('product', vm);
 });
 router.get('/:COMPANY/:ID', function (req, res, next) {
-    res.render('product', {ID: req.params.ID});
+    var log=req.session.isLogged;
+	var name=req.session.username;
+	var admin = false;
+	var vm = {
+		isAdmin: admin,
+		layout: false,
+		isLogged: log,
+		showError: false,
+		username: name
+	};
+    res.render('product', vm);
 });
 module.exports = router;
--- routes/shop.js
+++ routes/shop.js
@@ -1,9 +1,29 @@
 var express = require('express');
 var router = express.Router();
 router.get('/', function (req, res, next) {
-    res.render('shop');
+    var log=req.session.isLogged;
+	var name=req.session.username;
+	var admin = false;
+	var vm = {
+		isAdmin: admin,
+		layout: false,
+		isLogged: log,
+		showError: false,
+		username: name
+	};
+    res.render('shop', vm);
 });
 router.get('/:company', function (req, res, next) {
-    res.render('shop');
+    var log=req.session.isLogged;
+	var name=req.session.username;
+	var admin = false;
+	var vm = {
+		isAdmin: admin,
+		layout: false,
+		isLogged: log,
+		showError: false,
+		username: name
+	};
+    res.render('shop', vm);
 });
 module.exports = router;

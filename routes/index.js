8c74ee844ec6d40bac3edd019255d171bf729618
nhvn <nguyenhoangvannha@gmail.com>
2018-06-28 17:31:45 +0700

    Fix tnhan

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,17 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index');
+	var log=req.session.isLogged;
+	var name=req.session.username;
+	var admin = false;
+	var vm = {
+		isAdmin: admin,
+		layout: false,
+		isLogged: log,
+		showError: false,
+		username: name
+	};
+  res.render('index', vm);
 });
 
 module.exports = router;

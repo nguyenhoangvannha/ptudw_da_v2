6aa60bf49b4b193407a89c6afd6932a965522663
Hồ Thiện Nhân <36923040+HoThienNhan1997@users.noreply.github.com>
2018-06-28 15:45:24 +0700

    Cập nhật file accountRepo

--- repos/accountRepo.js
+++ repos/accountRepo.js
@@ -1,21 +1,21 @@
 var db = require('../fn/db');
 
 exports.add = user => {
-    var sql = `insert into nguoidung (EMAIL, USERNAME, PASSWORD, PERMISSION) values('${user.email}', '${user.username}', '${user.password}', 0)`;
+    var sql = `insert into tbl_nguoidung (EMAIL, USERNAME, PASSWORD, PERMISSION) values('${user.email}', '${user.username}', '${user.password}', 0)`;
     return db.save(sql);
 }
 
 exports.login = user => {
-    var sql = `select * from nguoidung where USERNAME = '${user.username}' and PASSWORD = '${user.password}'`;
+    var sql = `select * from tbl_nguoidung where USERNAME = '${user.username}' and PASSWORD = '${user.password}'`;
     return db.load(sql);
 }
 
 exports.load = user=> {
-    var sql = `select * from nguoidung where USERNAME = '${user}'`;
+    var sql = `select * from tbl_nguoidung where USERNAME = '${user}'`;
     return db.load(sql);
 }
 
 exports.update = user => {
-	var sql = `update nguoidung set LINKANH='${user.linkanh}', SODT='${user.sdt}', EMAIL='${user.email}', DIACHI='${user.address}' where USERNAME='${user.username}'`;
+	var sql = `update tbl_nguoidung set LINKANH='${user.linkanh}', SODT='${user.sdt}', EMAIL='${user.email}', DIACHI='${user.address}' where USERNAME='${user.username}'`;
     return db.save(sql);
 }
